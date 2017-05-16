8ffbef455afc4cc2d5d344a83636471de81224ca
love-adelar <love.adelar@gmail.com>
2017-05-16 16:43:26 +0900

    Changed image effects, and font color, etc

--- js/main.js
+++ js/main.js
@@ -9,9 +9,6 @@ $(document).ready(function(){
 
 	});
 	
-
-
-
 	$("#char22").hide();
 	$("#char33").hide();
 	$("#char44").hide();
@@ -20,21 +17,13 @@ $(document).ready(function(){
 		$("#char11").show();
 		$("#char22").hide();
 		$("#char33").hide();
-		$("#char44").hide();
-		$("img[id=img-22]").attr("src", "images/Char4.png");
-		$("img[id=img-33]").attr("src", "images/Char4.png");
-		$("img[id=img-44]").attr("src", "images/Char4.png");
-			
+		$("#char44").hide();	
 	});
 	$('#char1').mouseout(function() {
 		$("#char11").show();
 		$("#char22").hide();
 		$("#char33").hide();
 		$("#char44").hide();
-		$("img[id=img-22]").attr("src", "images/Char2.png");
-		$("img[id=img-33]").attr("src", "images/Char3.png");
-		$("img[id=img-44]").attr("src", "images/Char4.png");
-			
 	});
 
 	$('#char2').mouseover(function() {
@@ -42,54 +31,35 @@ $(document).ready(function(){
 		$("#char22").show();
 		$("#char33").hide();
 		$("#char44").hide();
-		$("img[id=img-11]").attr("src", "images/Char3.png");
-		$("img[id=img-33]").attr("src", "images/Char3.png");
-		$("img[id=img-44]").attr("src", "images/Char3.png");
 	});
 	$('#char2').mouseout(function() {
 		$("#char11").show();
 		$("#char22").hide();
 		$("#char33").hide();
 		$("#char44").hide();
-		$("img[id=img-11]").attr("src", "images/Char3.png");
-		$("img[id=img-33]").attr("src", "images/Char3.png");
-		$("img[id=img-44]").attr("src", "images/Char3.png");
-			
 	});	$('#char3').mouseover(function() {
 		$("#char11").hide();
 		$("#char22").hide();
 		$("#char33").show();
 		$("#char44").hide();
-		$("img[id=img-11]").attr("src", "images/Char4.png");
-		$("img[id=img-22]").attr("src", "images/Char4.png");
-		$("img[id=img-44]").attr("src", "images/Char4.png");
 	});
 	$('#char3').mouseout(function() {
 		$("#char11").show();
 		$("#char22").hide();
 		$("#char33").hide();
 		$("#char44").hide();
-		$("img[id=img-11]").attr("src", "images/Char4.png");
-		$("img[id=img-22]").attr("src", "images/Char4.png");
-		$("img[id=img-44]").attr("src", "images/Char4.png");	
 	});	
 	$('#char4').mouseover(function() {
 		$("#char11").hide();
 		$("#char22").hide();
 		$("#char33").hide();
 		$("#char44").show();
-		$("img[id=img-11]").attr("src", "images/Char2.png");
-		$("img[id=img-22]").attr("src", "images/Char2.png");
-		$("img[id=img-33]").attr("src", "images/Char2.png");
 	});
 	$('#char4').mouseout(function() {
 		$("#char11").hide();
 		$("#char22").hide();
 		$("#char33").hide();
 		$("#char44").show();
-		$("img[id=img-11]").attr("src", "images/Char2.png");
-		$("img[id=img-22]").attr("src", "images/Char2.png");
-		$("img[id=img-33]").attr("src", "images/Char2.png");
 	});
 
 
